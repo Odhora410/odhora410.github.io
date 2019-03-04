c4dd1840e4ff27069a9714ec591f60698dae6916
Odhora410 <31898282+Odhora410@users.noreply.github.com>
2019-03-04 00:43:54 -0600

    Delete gatsby-config.js

--- gatsby-tutorial-plugins-components-master/gatsby-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [`gatsby-plugin-typography`],
-    siteMetadata: {
-        title: `Gatsby tutorial`,
-        description: `A really awesome tutorial for GatsbyJs`,
-    },
-}

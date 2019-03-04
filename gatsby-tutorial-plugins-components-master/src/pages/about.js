c2154423d33f0feecb1d07a0d4035bc07999323b
Odhora410 <31898282+Odhora410@users.noreply.github.com>
2019-03-04 00:42:54 -0600

    Delete about.js

--- gatsby-tutorial-plugins-components-master/src/pages/about.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import Layout from "../components/layout"
-
-export default () => (
-    <Layout>
-        <h1>About</h1>
-        <p>Sonic landscapes drown out the doldrums of the every day.</p>
-    </Layout>
-)
